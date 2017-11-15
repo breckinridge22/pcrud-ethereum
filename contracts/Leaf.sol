f69b026ef1a05ce171d120864a654d2d4b574fe7
breckinridge <BreckStodghill@Breckinridges-MacBook-Pro.local>
2017-11-15 17:25:23 -0600

    Compiled and run successfully

--- contracts/Leaf.sol
+++ contracts/Leaf.sol
@@ -27,7 +27,7 @@ contract Leaf {
   // an Account which contains a public key and a list of balances
   struct Account {
     // corresponds to a stellar public key
-    uint public_key;
+    uint256 public_key;
     mapping(bytes32 => Balance) balances;
   }
 
@@ -41,13 +41,14 @@ contract Leaf {
   }
 
   // create a new Account
-  function createAccount(uint _public_key) onlyOwner {
+  // but if account already exists throw an error
+  function createAccount(uint256 _public_key) onlyOwner {
     Account memory a = Account(_public_key);
     accounts[_public_key] = a;
   }
 
   // add a balance to an account
-  function addBalanceToAccount(uint _public_key, bytes32 _assetCode, uint256 _amount) onlyOwner {
+  function addBalanceToAccount(uint256 _public_key, bytes32 _assetCode, uint256 _amount) onlyOwner {
     Balance memory balance = Balance(_assetCode, _amount);
     // check to make sure Account Id exists in accounts
     Account storage a = accounts[_public_key];
@@ -55,18 +56,26 @@ contract Leaf {
   }
 
   // updates a given balance for an account
-  function updateBalanceInAccount(uint _public_key, bytes32 _assetCode, uint _amount) onlyOwner{
+  function updateBalanceInAccount(uint256 _public_key, bytes32 _assetCode, uint _amount) onlyOwner{
     Account storage a = accounts[_public_key];
     Balance memory balance = Balance(_assetCode, _amount);
     a.balances[_assetCode] = balance;
   }
 
   // return the balance of a specific account, assetcode
-  function getBalance(uint _public_key, bytes32 _assetCode) onlyOwner returns (Balance){
+  function getBalance(uint256 _public_key, bytes32 _assetCode) onlyOwner returns (uint){
     Account storage a = accounts[_public_key];
     //require(_index <= a.balances.length);
     // require that the access code is in the mapping
-    return a.balances[_assetCode];
+    return a.balances[_assetCode].amount;
   }
 
+  function removeBalanceFromAccount(uint256 _public_key, bytes32 _assetCode) onlyOwner {
+    Account storage a = accounts[_public_key];
+    delete a.balances[_assetCode];
+  }
+
+  /*function deleteAccount(uint256 _public_key) onlyOwner {
+      delete accounts[_public_key];
+  }*/
 }
